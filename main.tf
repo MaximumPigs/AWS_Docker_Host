2f3ff92e6632b9c4453e95151dcfece1c890c18c
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-02-18 09:20:24 +1100

    correct typo

--- main.tf
+++ main.tf
@@ -26,12 +26,12 @@ resource "aws_instance" "my_instance" {
   }
 
   network_interface {
-    network_interface_id = aws_network_interface.honetpot_nic.id
+    network_interface_id = aws_network_interface.honeypot_nic
     device_index         = 0
   }
 }
 
-resource "aws_network_interface" "honetpot_nic" {
+resource "aws_network_interface" "honeypot_nic" {
   subnet_id       = aws_subnet.subnet.id
   private_ip      = "10.0.0.100"
   security_groups = ["${aws_security_group.security_group.id}"]
@@ -47,8 +47,8 @@ resource "aws_eip" "public" {
     aws_network_interface.honeypot_nic
   ]
 
-  network_interface         = aws_network_interface.honetpot_nic.id
-  associate_with_private_ip = aws_network_interface.honetpot_nic.private_ip
+  network_interface         = aws_network_interface.honeypot_nic
+  associate_with_private_ip = aws_network_interface.honeypot_nic.private_ip
   vpc                       = true
 }
 
