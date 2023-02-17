ab64480cd9f165f7c8cea53963444530af34a063
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-02-17 17:58:56 +1100

    AWS key environment variable update

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
   backend "s3" {
     region = "ap-southeast-2"
     bucket = "terraform-backend-maximumpigs"
-    key = "state/terraform.tfstate"
+    key    = "state/terraform.tfstate"
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-2"
+  region     = "ap-southeast-2"
   access_key = var.access_key
   secret_key = var.secret_key
 }
