7ca83eb4f7cdf0e8afb9799f2a7c396e91b6d332
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-02-18 07:53:01 +1100

    Add network fabric and ssh inbound

--- main.tf
+++ main.tf
@@ -19,5 +19,23 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami           = "ami-0692dea0a2f8a1b35"
   instance_type = "t2.micro"
-  key_name      = "MaximumPigs_Key_Pair"
+  key_name      = var.key_pair
+
+  tags = {
+    "name" = "Honeypot"
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.honetpot_nic.id
+    device_index         = 0
+  }
+}
+
+resource "aws_network_interface" "honetpot_nic" {
+  subnet_id   = aws_subnet.subnet.id
+  private_ips = ["10.0.0.100"]
+
+  tags = {
+    "name" = "primary_network_interface"
+  }
 }
---
+++ network_fabric.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    "name" = "honeypot_vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    "name" = "honeypot_subnet"
+  }
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "honeypot_security_group"
+  description = "firewall rules for honeypot"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    cidr_blocks = var.my_ip
+    to_port     = 22
+    protocol    = "ssh"
+  }
+
+  egress {
+    description = "all outbound"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = -1
+  }
+}
