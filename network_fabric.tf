1e366cc8bddcbb8400007d9a781f6ada1f846465
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-02-19 20:26:02 +1100

    Remove local route from default table

--- network_fabric.tf
+++ network_fabric.tf
@@ -27,11 +27,6 @@ resource "aws_default_route_table" "route" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
-    cidr_block = aws_vpc.vpc.cidr_block
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
