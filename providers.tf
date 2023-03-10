afcfee4ca7dcf418216c810c0c58a93829470bf8
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-03-11 07:49:26 +1100

    Remove random provider

--- providers.tf
+++ providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0.0"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.4"
