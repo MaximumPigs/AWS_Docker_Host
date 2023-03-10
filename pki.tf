13184e6afee31a38c8d41681d2f7126d3c531c1c
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-03-10 16:40:06 +1100

    Update pki.tf

--- pki.tf
+++ pki.tf
@@ -12,6 +12,6 @@ resource "aws_secretsmanager_secret" "generated_key" {
 }
 
 resource "aws_secretsmanager_secret_version" "generated_key" {
-  secret_id = aws_secretsmanager_secret.generated_key.id
+  secret_id     = aws_secretsmanager_secret.generated_key.id
   secret_string = tls_private_key.terraform.private_key_pem
 }
